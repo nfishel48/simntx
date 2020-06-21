6238384e9963ec55bac58b87bfdbcd63d4a986ba
Nick <nick.gilliam2@gmail.com>
2020-06-20 20:54:37 -0400

    Changed the section slider for the last time

--- static_in_env/js/listings.js
+++ static_in_env/js/listings.js
@@ -1,39 +1,24 @@
 $(document).ready(function(){
 	$('.slide-left').on('click', function(){
-		$(this).siblings('.slider').css('margin-left', '0');
+		$(this).closest('.section').find('.slider').css('margin-left', '0');
 		
 		$(this).siblings('.slide-right').css('display', 'flex');
 		$(this).css('display', 'none');
 	});
 	$('.slide-right').on('click', function(){
-		var outer = $(this).parent().parent().width(),
-			inner = $(this).siblings('.slider').width();
+		var outerDiv = $(this).closest('.section').find('.section-slide'),
+			innerDiv = $(this).closest('.section').find('.slider');
+		
+		var outer = outerDiv.width(),
+			inner = innerDiv.width();
 		
 		var width = inner - outer;
 		
-		$(this).siblings('.slider').css('margin-left', (0 - width) + 'px');
+		console.log($(outerDiv).width());
+		
+		$(innerDiv).css('margin-left', (0 - width) + 'px');
 		
 		$(this).siblings('.slide-left').css('display', 'flex');
 		$(this).css('display', 'none');
-	});
-	
-	$('.move-left').on('click', function(){
-		$(this).parent().parent().parent().find('.slider').css('margin-left', '0');
-		
-		$(this).siblings('.move-right').css('opacity', '1');
-		$(this).css('opacity', '.5');
-	});
-	$('.move-right').on('click', function(){
-		var outer = $(this).parent().parent().parent().find('.section-slide').width(),
-			inner = $(this).parent().parent().parent().find('.slider').width();
-		
-		var width = inner - outer;
-		
-		console.log($(this).parent().parent().parent().find('.section-slide').width());
-		
-		$(this).parent().parent().parent().find('.slider').css('margin-left', (0 - width) + 'px');
-		
-		$(this).siblings('.move-left').css('opacity', '1');
-		$(this).css('opacity', '.5');
 	});
 });
