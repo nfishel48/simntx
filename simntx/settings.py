208ae45c048a60c9a83f077634f8b1234a037198
Nick <nick.gilliam2@gmail.com>
2020-06-23 16:42:25 -0400

    Added back allowed host

--- simntx/settings.py
+++ simntx/settings.py
@@ -5,7 +5,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = ['localhost', '10.0.0.53', 'simntxdev.herokuapp.com',  '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '10.0.0.53', 'simntxdev.herokuapp.com', 'simntx.herokuapp.com', '127.0.0.1']
 
 # must make migrations and migrate after adding apps
 INSTALLED_APPS = [
