4450212a2b7e563fa1caea709c1c1d8ba6567d94
Nathan Fishel <nfishel@Nathans-MacBook-Air.local>
2020-07-02 14:03:58 -0400

    added customer options too the orders screen

--- core/models.py
+++ core/models.py
@@ -175,6 +175,7 @@ class Order(models.Model):
 
 
 
+
 class Address(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     street_address = models.CharField(max_length=100)
