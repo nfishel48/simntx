c819117d3cf7edc1e86ee303f2822f8b06a5e687
Nick <nick.gilliam2@gmail.com>
2020-07-02 22:13:40 -0400

    Altering the order model slightly reducing redundancy

--- core/models.py
+++ core/models.py
@@ -93,10 +93,7 @@ class Item(models.Model):
 
 
 class OrderItem(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    ordered = models.BooleanField(default=False)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name = 'item')
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
@@ -114,31 +111,31 @@ class OrderItem(models.Model):
     def get_final_price(self):
         if self.item.discount_price:
             return self.get_total_discount_item_price()
+
         return self.get_total_item_price()
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    ref_code = models.CharField(max_length=20, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    driver = models.ForeignKey('UserProfile', related_name='driver', on_delete=models.SET_NULL, default=1, blank=True, null=True)
     items = models.ManyToManyField(OrderItem)
+
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
+
+    shipping_address = models.ForeignKey('Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
+    billing_address = models.ForeignKey('Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
+
+    payment = models.ForeignKey('Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey('Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    ref_code = models.CharField(max_length=20, blank=True, null=True)
+
     ordered = models.BooleanField(default=False)
-    shipping_address = models.ForeignKey(
-        'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
-    billing_address = models.ForeignKey(
-        'Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
-    payment = models.ForeignKey(
-        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
-    coupon = models.ForeignKey(
-        'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
     being_delivered = models.BooleanField(default=False)
     received = models.BooleanField(default=False)
+
     refund_requested = models.BooleanField(default=False)
     refund_granted = models.BooleanField(default=False)
-    #driver = models.CharField(default="None", max_length=20)
-    driver = models.ForeignKey('UserProfile', related_name='driver', on_delete=models.SET_NULL, default = 1, blank=True, null=True)
 
     '''
     1. Item added to cart
@@ -172,8 +169,6 @@ class Order(models.Model):
         return reverse("core:order", kwargs={
             'ref_code': self.ref_code
         })
-
-
 
 
 class Address(models.Model):
