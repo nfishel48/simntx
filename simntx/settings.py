a339369746f43ad7f3770071397aa930cdfdb7c3
Nathan Fishel <nfishel@emich.edu>
2020-07-10 13:58:24 -0400

    installed white nosie for servering static files

--- simntx/settings.py
+++ simntx/settings.py
@@ -52,7 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'simntx.middleware.auth_req.AuthRequiredMiddleware'
+    'simntx.middleware.auth_req.AuthRequiredMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 TEMPLATES = [
