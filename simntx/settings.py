b81a09134178dc85070f211632f58be821ff45e7
Nathan Fishel <nfishel@emich.edu>
2020-07-19 00:01:03 -0400

    fixed s3

--- simntx/settings.py
+++ simntx/settings.py
@@ -194,7 +194,8 @@ AWS_STORAGE_BUCKET_NAME = 'simntx'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'us-east-2'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
